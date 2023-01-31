20a91cf2c675dc18cd69aa89dffb99d8fce2aa30
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-31 15:53:07 -0500

    added home page cypress testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,6 +10,6 @@ describe('Home', () => {
   })
 
   it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
-  });
+    cy.get(".products article").should("have.length", 2)
+  })
 })
